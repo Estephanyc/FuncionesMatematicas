48246ce6a6bb25c0ca6dc746e5845a46ee44b819
AriellaRogueN <maitilydevidasi@gmail.com>
2018-05-26 14:58:42 -0400

    Agregue index.html y app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.write("El resultado de 36325 * (9824 + 777) es " + multiply(36325, add(9824, 777)));
+function add(number1, number2) {
+    return number1 + number2;
+}
+function multiply(number1, number2) {
+    return number1 * number2;
+}
